35307d5f17bfc6f87e17e215318072ad98a74cc0
InkCN <201462457@qq.com>
2020-11-19 22:50:37 +0800

    添加了判断数组是否有序的方法

--- MySort/src/Test.java
+++ MySort/src/Test.java
@@ -3,15 +3,12 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
 
-//        ISort quickSort = new QuickSort();
-//        printResult(quickSort);//打印快排算法
+//        printResult(new QuickSort());//打印快排算法
 
-//        ISort mergeSort = new MergeSort();
-//        printResult(mergeSort);//打印归并算法
+        printResult(new MergeSort());//打印归并算法
 
 
     }
-
 
     public static int[] getArray(int n) {
         int[] arr = new int[n];
@@ -21,10 +18,44 @@ public class Test {
         return arr;
     }
 
+    /**判断数组是否有序
+     * 判断两次，先判断是否是升序(1..n)
+     * 再判断是否是降序(n..1)
+     * @param data 需要判断的数组
+     * @return
+     */
+    public static boolean isSorted(int[] data) {
+        boolean flag1 = false,flag2 = false;
+        //数组是否为升序
+        for (int i = 0; i < data.length-1; i++) {
+            if (data[i] == Math.min(data[i], data[i + 1])) {
+                flag1 = true;
+            } else {
+                flag1 = false;
+                break;
+            }
+        }
+        //数组是否为降序
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i] == Math.max(data[i], data[i + 1])) {
+                flag2 = true;
+            } else {
+                flag2 = false;
+                break;
+            }
+        }
+        if (flag1 || flag2) {
+            return true;//有一个为真，即是已经排过序了
+        } else {
+            return  false;
+        }
+    }
+
     public static void printResult(ISort sort){
         int[] arr = Test.getArray(15);
         System.out.println("排序前：" + Arrays.toString(arr));
         sort.sort(arr);
         System.out.println("排序后：" + Arrays.toString(arr));
+        System.out.print("是否有序：" + isSorted(arr));
     }
 }
