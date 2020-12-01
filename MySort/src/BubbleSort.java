156a9265a5d467014ab6832bcced5191584b2661
InkCN <201462457@qq.com>
2020-12-01 17:54:40 +0800

    冒泡排序

--- MySort/src/BubbleSort.java
+++ MySort/src/BubbleSort.java
@@ -8,11 +8,11 @@ public class BubbleSort implements ISort {
 
     private static void bubbleSort(int[] arr) {
         int N = arr.length;
-        for (int i = 0; i < N; i++) {
-            for (int j = 1; j < N-i; j++) {
-                if (arr[j - 1] > arr[j]) {
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
+        for (int i = 0; i < N-1; i++) {
+            for (int j = 0; j < N-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j +1];
+                    arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
             }
