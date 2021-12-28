fab39cf3f5ce9b62152b5de00d0e2d6599e24d3c
Your Name <you@example.com>
2021-12-28 16:32:57 +0800

    correct a mistake

--- MySort/src/ShellSort.java
+++ MySort/src/ShellSort.java
@@ -10,7 +10,7 @@ public class ShellSort implements ISort{
         while(h<N/3) h=3*h+1;
         while(h>=1){
             for(int i=h;i<N;i++){
-                for(int j=i;j>=h&&arr[j]<arr[j-1];j-=h){
+                for(int j=i;j>=h&&arr[j]<arr[j-h];j-=h){
                     int temp=arr[j];
                     arr[j]=arr[j-h];
                     arr[j-h]=temp;
